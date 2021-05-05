501e400f0e01593707122a6e95ecc14df0bab09b
Brandon <omstachu626@gmail.com>
2021-05-05 12:53:53 -0700

    finished event handler for player name and new game button

--- connect-four.js
+++ connect-four.js
@@ -8,11 +8,26 @@ window.addEventListener("DOMContentLoaded", event => {
     const p2Name = document.getElementById("player-2-name");
     const newGame = document.getElementById("new-game");
 
-    p1Name.addEventListener("keyup", event => {
-        newGame.disabled = false;
-        if (p1Name.value && p2Name.value) {
+    let checkName = function(player) {
+        player.addEventListener("keyup", event => {
             newGame.disabled = true;
-        }
+            if (p1Name.value !== '' && p2Name.value !== '') {
+                newGame.disabled = false;
+            }
+
+        })
+    }
+
+    checkName(p1Name);
+    checkName(p2Name);
+
+    newGame.addEventListener("click", event => {
+        game = new Game(p1Name, p2Name)
+        p1Name.value = ""
+        p2Name.value = ""
+        newGame.disabled = true;
+        updateUI();
     })
+
 
 })
