63b99b9f40bc7f925465436ced68118ea394a633
Brandon <omstachu626@gmail.com>
2021-05-06 14:05:27 -0700

    finished column win inspector

--- column-win-inspector.js
+++ column-win-inspector.js
@@ -5,6 +5,12 @@ export default class ColumnWinInspector {
     }
 
     inspect() {
-        
+        for (let i = 5; i >= 3; i--) {
+            let row = this.columnObj.rows;
+            if (row[i] === row[i - 1] === row[i - 2] === row[i - 3]) {
+                return row[i];
+            }
+        }
+        return 0;
     }
 }
